cffa4ced81b873adc6eaa0916cca60afc5d61ef9
Utkarsh <utkarsh17verma@gmail.com>
2021-09-18 11:09:57 +0530

    Header, Footer and Home page Added

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const db = require('./../modules/dbConnection');
 
 
 // all routes are relative to /
-router.get('/',(req,res)=>{
-    res.send("No Home Page")
-})
+router.get('/',function (req,res, next) {
+    res.render("Home/index");
+});
 
 module.exports = router;
