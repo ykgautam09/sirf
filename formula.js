c3ab2ff96ff10e24d18ea522cbf6494c980659d7
Vijay <vijayrauniyar1818@gmail.com>
2022-02-09 20:04:33 +0530

    Ranking formula created

--- formula.js
+++ formula.js
@@ -3,8 +3,9 @@ function f(A,B){
     return A/B;
 }
 
+
 // Student Strength function
-function studentStrength(section,NT,NE,NP) {
+function SSfunction(section,NT,NE,NP) {
     var SS;
     if(section.toLowerCase()=="architecture"){
         SS=f(NT,NE)*20;
@@ -15,13 +16,16 @@ function studentStrength(section,NT,NE,NP) {
     return SS;
 }
 
-// faculty student ratio
-function facultyStudentratio(section,F,N,NP) {
+
+
+// faculty student ratio FSR
+function FSRfunction(section,F,N,NP) {
+    section=section.toLowerCase();
     var ratio;
     var FSR;
 
     // for architecture and management
-    if(section.toLowerCase()=="architecture"||section.toLowerCase()=="management "){
+    if(section=="architecture"||section=="management"||section=="college"){
         ratio=F/N;
         FSR = 30*(15*(F/N))
     }
@@ -33,7 +37,8 @@ function facultyStudentratio(section,F,N,NP) {
        FSR = 30*(15*(F/N))
     } 
 
-switch(section.toLowerCase()) {
+    // ratio comparison to score score maximum marks required in each case
+switch(section) {
         case "architecture ":
             if(ratio<1/50){
                 FSR=0;
@@ -64,6 +69,11 @@ switch(section.toLowerCase()) {
                 FSR=0;
             }
           break;
+        case "college":
+            if(ratio<1/70){
+                FSR=0;
+            }
+          break;
         default:
             break;
 
@@ -72,4 +82,103 @@ switch(section.toLowerCase()) {
 }
 
 
+
+
+
+
+
+// calculating rank here
+function Ranking(section){
+    var ranking;
+    switch(section.toLowerCase()) {
+
+        case "architecture ":
+        var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+        var RPC = 0.6*PU + 0.2*QP + 0*IPR + 0.2*FPPP;
+        var GO = 0.4*GPH + 0.3*GUE + 0.3*GMS + 0*GPHD + 0*GSS;
+        var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+        var PR = 1;
+
+        ranking = 0.4*TLR + 0.2*RPC + 0.2*GO + 0.1*OI + 0.1*PR;
+          break;
+
+        case "management":
+            var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+            var RPC = 0.4*PU + 0.4*QP + 0*IPR + 0.2*FPPP;
+            var GO = 0.4*GPH + 0.2*GUE + 0.4*GMS + 0*GPHD + 0*GSS;
+            var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+            var PR = 1;
+            ranking = 0.3*TLR + 0.3*RPC + 0.2*GO + 0.1*OI + 0.1*PR;
+
+          break;
+
+
+        case "engineering":
+            var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+            var RPC = 0.35*PU + 0.4*QP + 0.15*IPR + 0.1*FPPP;
+            var GO = 0.4*GPH + 0.15*GUE + 0.25*GMS + 0.2*GPHD + 0*GSS;
+            var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+            var PR = 1;
+            ranking=0.3*TLR + 0.3*RPC + 0.2*GO + 0.1*OI + 0.1*PR;
+          break;
+
+
+        case "pharmacy":
+            var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+            var RPC = 0.35*PU + 0.4*QP + 0.15*IPR + 0.1*FPPP;
+            var GO = 0.4*GPH + 0.15*GUE + 0.25*GMS + 0.2*GPHD + 0*GSS;
+            var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+            var PR = 1;
+            ranking = 0.3*TLR + 0.3*RPC + 0.2*GO + 0.1*OI + 0.1*PR;
+            
+          break;
+
+
+        case "law":
+            var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+            var RPC = 0.5*PU + 0.3*QP + 0*IPR + 0.2*FPPP;
+            var GO = 0.4*GPH + 0.15*GUE + 0.25*GMS + 0.2*GPHD + 0*GSS;
+            var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+            var PR = 1;
+            ranking = 0.4*TLR + 0.15*RPC + 0.25*GO + 0.1*OI + 0.1*PR;
+
+          break;
+
+
+        case "medical":
+            var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+            var RPC = 0.4*PU + 0.4*QP + 0.1*IPR + 0.1*FPPP;
+            var GO = 0.3*GPH + 0.3*GUE + 0*GMS + 0.3*GPHD + 0.1*GSS;
+            var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+            var PR = 1;
+            
+            ranking = 0.3*TLR + 0.3*RPC + 0.2*GO + 0.1*OI + 0.1*PR;
+          break;
+
+
+        case "college":
+        var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+        var RPC = 0.7*PU + 0.3*QP + 0*IPR + 0*FPPP;
+        var GO = 0.4*GPH + 0.4*GUE + 0.2*GMS + 0*GPHD + 0*GSS;
+        var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+        var PR = 1
+          
+          ranking = 0.4*TLR + 0.15*RPC + 0.25*GO + 0.1*OI + 0.1*PR
+          break;
+
+        case "overall":
+        var TLR = 0.2*SS + 0.3*FSR + 0.2*FQE + 0.3*FRU;
+        var RPC = 0.35*PU + 0.35*QP + 0.15*IPR + 0.15*FPPP;
+        var GO = 0*GPH + 0.6*GUE + 0*GMS + 0.4*GPHD + 0*GSS;
+        var OI = 0.3*RD + 0.3*WD + 0.2*ESCS + 0.2*PCS;
+        var PR = 1;
+        ranking = 0.3*TLR + 0.3*RPC + 0.2*GO + 0.1*OI + 0.1*PR;          
+          break;
+
+        default:
+            break;
+      }
+
+ return ranking;
+}
 
