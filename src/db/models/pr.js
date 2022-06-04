5e3f2d3fe609f86f1358626f2ffc3300f5eda0b7
ykgautam09 <yateeshgautam1357@gmail.com>
2022-06-04 17:34:54 +0530

    add Perception database migration tables

---
+++ src/db/models/pr.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class pr extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  pr.init(
+    {
+      year: DataTypes.INTEGER,
+      institute_id: DataTypes.INTEGER,
+      pr: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: "pr",
+      underscored: true
+    }
+  );
+  return pr;
+};
