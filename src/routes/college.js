b28072821586f75f38263da63c1d3e9745667cfb
ykgautam09 <yateeshgautam1357@gmail.com>
2022-06-05 13:17:46 +0530

    add forgot password functionality

--- src/routes/college.js
+++ src/routes/college.js
@@ -10,26 +10,6 @@ const db = require("./../db/models/index");
 const Institute = db.Institute;
 const OTP = db.OTP;
 
-// college login
-router.get("/login", (req, res) => {
-  res.render("College/login");
-});
-
-// college forgot Password
-router.get("/forgot-password", (req, res) => {
-  res.render("College/forgot_password");
-});
-
-// college enter OTP
-router.get("/enter-otp", (req, res) => {
-  res.render("College/enter_otp");
-});
-
-// college register
-router.get("/register", (req, res) => {
-  res.render("College/register");
-});
-
 // accept user data from register page
 router.post(
   "/register",
@@ -107,12 +87,13 @@ router.post(
     } catch (err) {
       console.log(err);
       await transaction.rollback();
-      res.send("Something gone wrong" + err);
+      res.send("Something went wrong!" + err);
     }
   }
 );
 
-router.get("/verify/:email", async (req, res) => {
+// verify registered mail
+router.get("/:path/:email", async (req, res) => {
   const transaction = await db.sequelize.transaction();
   try {
     let email = req.params.email;
@@ -170,6 +151,12 @@ router.get("/verify/:email", async (req, res) => {
   }
 });
 
+// college login
+router.get("/login", (req, res) => {
+  res.render("College/login");
+});
+
+// login using password and email
 router.post("/login", async (req, res) => {
   try {
     let email = req.body.userEmail;
@@ -204,8 +191,130 @@ router.post("/login", async (req, res) => {
     return res.render("College/dashboard");
   } catch (err) {
     console.log("something goes wrong", err);
-    res.send(err.toString());
+    res.send("Credentials Doesn't Matched!");
   }
+});
+
+// college forgot Password
+router.get("/forgot-password", (req, res) => {
+  res.render("College/forgot_password");
+});
+
+router.post("/forgot-password", async (req, res) => {
+  try {
+    let email = req.body.email;
+
+    const institute = await Institute.findOne({ where: { email } });
+    if (institute.id) {
+      console.log("user exists in db");
+
+      let otp = cred.genOtp();
+      // save otp in otp table
+      const password = await OTP.create({
+        code: otp,
+        institute_id: institute.id,
+        expiry_at: addMinutes(new Date().getTime(), 2)
+      }); // otp valid for 2 min
+      // generate verification link
+      let verificationLink = `${req.protocol}://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/college/password-reset/${email}/?code=${otp}`;
+
+      // read mailing template to send login notification
+      ejs
+        .renderFile(
+          path.join(__dirname, "..", "views", "Mail", "registration.ejs"),
+          {
+            verificationLink,
+            email
+          }
+        )
+        .then(async (mailTemplate) => {
+          // send user email with login details
+          let mailOption = {
+            subject: "UPIRF:Password Reset Request",
+            to: email,
+            from: process.env.MAILING_ID,
+            html: mailTemplate
+          };
+
+          const mailRes = await sendMail(mailOption);
+          // send user to dashboard even if login notification wasn't sent to mail
+          if (!mailOption) throw new Error("Couldn't sent otp");
+        });
+    } else throw new Error("Couldn't find user");
+
+    return res.render("College/enter_otp");
+  } catch (err) {
+    console.log("something goes wrong", err);
+    res.send("Something went wrong!");
+  }
+});
+
+// college enter OTP
+router.get("/enter-otp", (req, res) => {
+  res.render("College/enter_otp");
+});
+
+router.post("/enter-otp", async (req, res) => {
+  const transaction = await db.sequelize.transaction();
+  try {
+    let email = req.post.email;
+    let pass = req.query.code;
+
+    // read otp created less than 2 min ago
+    const institute = await Institute.findOne({ where: { email } });
+    if (!institute) throw new Error("User doesn't exist");
+    const otpRes = await OTP.findOne({
+      where: { expiry_at: { [db.Sequelize.Op.gte]: db.Sequelize.fn("NOW") } },
+      institute_id: institute.id
+    });
+    let otp = otpRes.code;
+    if (otp === pass) {
+      console.log("account verified successfully");
+      // asynchronously delete stored otp once used Successfully
+      await OTP.destroy(
+        { where: { institute_id: institute.id, code: otp } },
+        { transaction }
+      );
+    }
+    // generate user password after user verification
+    let password = cred.genPassword();
+    let passHash = cred.genHash(password);
+
+    // account verified and set password to set account active
+    await Institute.update(
+      { password: passHash },
+      { where: { id: institute.id } },
+      { transaction }
+    );
+    // read mailing template to send credentials back to user
+    ejs
+      .renderFile(
+        path.join(__dirname, "..", "views", "Mail", "credential.ejs"),
+        { email, pass: password }
+      )
+      .then(async (mailTemplate) => {
+        // send user email with credentials
+        let mailOption = {
+          subject: "Account Activated Successfully",
+          to: email,
+          from: process.env.MAILING_ID,
+          html: mailTemplate
+        };
+        const mailRes = await sendMail(mailOption);
+        if (!mailOption) {
+          throw new Error(mailRes.toString());
+        }
+      });
+    return res.render("College/login");
+  } catch (err) {
+    console.log("link doesn't matched with database", err);
+    res.send("Either link is already used or it is expired");
+  }
+});
+
+// college register
+router.get("/register", (req, res) => {
+  res.render("College/register");
 });
 
 // dashboard
